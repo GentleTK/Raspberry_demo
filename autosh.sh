bf3889a50fb7445b52002c3dde802a024e4906b2
GentleTK <1144626145@qq.com>
2019-06-06 09:20:02 +0800

    modified autosh

--- autosh.sh
+++ autosh.sh
@@ -1,4 +1,4 @@
 #run inquiry-with-rssi.py range distance
-#sudo python /home/pi/Raspberry_demo/inquiry-with-rssi.py
+sudo python /home/pi/Raspberry_demo/inquiry-with-rssi.py
 #Test run sendmail.py to send GPS Info E-Mail
-sudo python /home/pi/Raspberry_demo/sendmail.py
+#sudo python /home/pi/Raspberry_demo/sendmail.py
