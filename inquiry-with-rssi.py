aa2cb6e34e24a7197f813ba311e769d8d1e3d339
LYL <38547725+GentleTK@users.noreply.github.com>
2019-06-07 23:42:49 +0800

    Update inquiry-with-rssi.py
    add home coordinate

--- inquiry-with-rssi.py
+++ inquiry-with-rssi.py
@@ -142,12 +142,12 @@ def device_inquiry_with_with_rssi(sock):
                         #if report['class'] == 'TPV':
                         #    y = report.lon
                         #    x = report.lat
-						#E601 Coordinate
+			#E601 Coordinate
                         #y = 112.926
                         #x = 27.8505
-						#Home Coordinate
-						y = 113.5241
-						x = 26.769
+			#Home Coordinate
+			y = 113.5241
+			x = 26.769
                         #change GPS coordinate
                         loc=wgs2bd(x,y)
                         #write GPS Information to csvfile
