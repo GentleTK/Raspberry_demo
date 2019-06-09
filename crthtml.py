958211ab94e8a3480b8a7b0966f6d46f2c65977c
GentleTK <1144626145@qq.com>
2019-06-09 19:59:38 +0800

    add def create_html

--- crthtml.py
+++ crthtml.py
@@ -1,3 +1,4 @@
+#coding=UTF-8
 GEN_HTML = "MarkPoint.html"
 f = open(GEN_HTML,"w")
 message = """
@@ -7,7 +8,7 @@ message = """
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <style type="text/css">
         body, html{width: 100%;height: 100%;margin:0;font-family:"微软雅黑";}
-		#allmap{height:100%;width:100%;}
+	#allmap{height:100%;width:100%;}
     </style>
     <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=wgYlWnYQSZ5fN3RdCxYoIF8jT7y1jRLb"></script>
     <title>Map Marking</title>
@@ -22,9 +23,10 @@ message = """
 	 // 用经纬度设置地图中心点
     function theLocation(){
         map.clearOverlays();
-		//GPS to BD Coordinate
-		var new_point = new BMap.Point(113.5359, 26.7725);
-		//var new_point = new BMap.Point(1ng, lat);
+	//GPS to BD Coordinate
+	//var new_point = new BMap.Point(113.5359, 26.7725);
+	var new_point = new BMap.Point(112.9378, 27.8533);
+	//var new_point = new BMap.Point(1ng, lat);
         var marker = new BMap.Marker(new_point);  // 创建标注
         map.addOverlay(marker);              // 将标注添加到地图中
         map.panTo(new_point);      
--- inquiry-with-rssi.py
+++ inquiry-with-rssi.py
@@ -142,12 +142,12 @@ def device_inquiry_with_with_rssi(sock):
                         #if report['class'] == 'TPV':
                         #    y = report.lon
                         #    x = report.lat
-						#E601's Coordinate
+			#E601's Coordinate
                         #y = 112.926
                         #x = 27.8505
-						#Home's Coordinate
-						y = 113.5241
-						x = 26.769
+			#Home's Coordinate
+			y = 113.5241
+			x = 26.769
                         #change GPS coordinate
                         loc=wgs2bd(x,y)
                         #write GPS Information to csvfile
@@ -238,6 +238,47 @@ def wgs2bd(lat,lon):
     wgs_to_gcj = wgs2gcj(lat,lon)
     gcj_to_bd = gcj2bd(wgs_to_gcj[0], wgs_to_gcj[1])
     return gcj_to_bd;
+#create html
+def create_html():
+    GEN_HTML = "MarkPoint.html"
+    f = open(GEN_HTML,"w")
+    message = """
+    <html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <style type="text/css">
+        body, html{width: 100%;height: 100%;margin:0;font-family:"微软雅黑";}
+	#allmap{height:100%;width:100%;}
+    </style>
+    <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=wgYlWnYQSZ5fN3RdCxYoIF8jT7y1jRLb"></script>
+    <title>Map Marking</title>
+    </head>
+    <body>
+    <p id="allmap"></p>
+    <script type="text/javascript">
+    // 百度地图API功能
+    var map = new BMap.Map("allmap");
+    map.centerAndZoom(new BMap.Point(116.331398,39.897445),11);
+    map.enableScrollWheelZoom(true);
+	 // 用经纬度设置地图中心点
+    function theLocation(){
+        map.clearOverlays();
+	//GPS to BD Coordinate
+	//var new_point = new BMap.Point(113.5359, 26.7725);
+	var new_point = new BMap.Point(112.9378, 27.8533);
+	//var new_point = new BMap.Point(1ng, lat);
+        var marker = new BMap.Marker(new_point);  // 创建标注
+        map.addOverlay(marker);              // 将标注添加到地图中
+        map.panTo(new_point);      
+    }
+	document.getElementById("theLocation").innerHTML = theLocation();
+    </script>
+    </body>
+    </html>
+    """
+    f.write(message)
+    f.close()
 dev_id = 0
 try:
     sock = bluez.hci_open_dev(dev_id)
